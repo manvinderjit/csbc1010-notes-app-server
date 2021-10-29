c60edd8bb0e05a653c0138bae37325af59872418
manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2021-10-29 01:32:27 -0400

    master

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const config = require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const mysql = require('mysql')
 
 const healthRouter = require("./routes/health")
 const notesRouter = require("./routes/notes")
@@ -27,10 +28,53 @@ app.use(express.urlencoded({ extended: true }))
 /*
   TODO-1: Settup Database connection
 */
+// create connection to database
+
+// define database parameters
+const db = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db_notes'
+});
+
+
+// connect to database
+db.connect((err) => {
+  if (err) {
+      throw err;
+  }
+  console.log('Connected to database');  
+});
+  
 
 /*
   TODO-2: Upon database connection success, create the relavent table(s) if it does not exist.
 */
+
+// define query for table creation
+  const queryCreateTableNotes = `CREATE TABLE IF NOT EXISTS tb_notes(
+    id INT primary key auto_increment,
+    text varchar(200) NOT NULL,
+    dateCreated DATE NOT NULL,
+    lastModified TIMESTAMP NOT NULL
+  )`;
+
+// execute the query for table creation
+  db.query(queryCreateTableNotes, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+  
+ // closes connection though not required 
+  /*db.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+  });*/
+
+global.db = db;
 
 app.get('/', (req, res) => {
   res.send('CSBC1010 Assignment 3 - My Notes')
@@ -43,3 +87,5 @@ app.use("/note", noteRouter)
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
+
+module.exports = db;
